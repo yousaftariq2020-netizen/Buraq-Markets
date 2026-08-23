402988f9492e2aa02b15fd1f65d49fff02700c58
yousaftariq2020-netizen <yousaftariq2020@gmail.com>
2026-08-24 00:29:46 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const SUPABASE_URL = 'https://zlmoqiulzrqpnbxfeetp.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable__t2-g_ly7mOyzjOj6un36A_edOp9mU-';
